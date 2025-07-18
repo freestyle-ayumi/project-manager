dba198633a2fc3c10a509a3cacf955a07b507764
freestyle-ayumi <iwasaki.freestyle.music@gmail.com>
2025-07-18 16:03:11 +0900

    comit

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -49,6 +49,9 @@
                                             見積額
                                         </th>
                                         <th scope="col" class="px-2 py-2 text-left font-medium uppercase tracking-wider">
+                                            納品
+                                        </th>
+                                        <th scope="col" class="px-2 py-2 text-left font-medium uppercase tracking-wider">
                                             請求額
                                         </th>
                                         <th scope="col" class="px-2 py-2 text-left font-medium uppercase tracking-wider">
@@ -58,7 +61,7 @@
                                             実利
                                         </th>
                                         <th scope="col" class="relative px-2 py-2">
-                                            <span class="sr-only">Actions</span>
+                                            操作
                                         </th>
                                     </tr>
                                 </thead>
@@ -102,7 +105,7 @@
                                                     タスクなし
                                                 @endforelse
                                             </td>
-                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500">
+                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500 text-right">
                                                 @if (isset($latestQuotes[$project->id]))
                                                     <span class="flex items-center">
                                                         <a href="{{ route('quotes.show', $latestQuotes[$project->id]->id) }}" class="text-blue-500 hover:text-blue-700">
@@ -136,7 +139,9 @@
                                                 @endif
                                             </td>
 
-                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500">
+                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500 text-right">-</td>
+
+                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500 text-right">
                                                 @if ($project->invoices_sum_total_amount)
                                                     <a href="{{ route('invoices.index', ['project_id' => $project->id]) }}" class="text-blue-500 hover:text-blue-700">
                                                         ¥{{ number_format($project->invoices_sum_total_amount) }}
@@ -145,11 +150,11 @@
                                                     ¥0
                                                 @endif
                                             </td>
-                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500">
+                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500 text-right">
                                                 {{-- total_approved_expenses_sum は withSum(['expenses as total_approved_expenses_sum' => ...]) で定義されたもの --}}
                                                 ¥{{ number_format($project->total_approved_expenses_sum ?? 0) }}
                                             </td>
-                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500">
+                                            <td class="px-2 py-1 whitespace-nowrap text-sm text-gray-500 text-right">
                                                 @php
                                                     $netProfit = ($project->invoices_sum_total_amount ?? 0) - ($project->total_approved_expenses_sum ?? 0);
                                                 @endphp
@@ -160,9 +165,8 @@
                                                 {{-- Flexboxでアイコンを横並びに配置 --}}
                                                 <div class="flex items-center justify-end space-x-2"> {{-- ここを修正 --}}
                                                     <a href="{{ route('projects.edit', $project) }}" class="text-emerald-600 hover:text-emerald-400" title="編集">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                                            <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z"/>
                                                         </svg>
                                                     </a>
                                                     <form action="{{ route('projects.destroy', $project) }}" method="POST" class="inline-block" onsubmit="return confirm('本当にこのプロジェクトを削除しますか？');">
